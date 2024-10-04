303f3efda20399fd5e1ffc6bebb6577959e595ee
MrEyvaz <eyvaz.aliyev.7740@gmail.com>
2024-10-04 21:29:57 +0400

    Initial Commit

--- src/components/Firebase-datas/FireBase.jsx
+++ src/components/Firebase-datas/FireBase.jsx
@@ -1,16 +1,6 @@
 import { initializeApp } from 'firebase/app'; 
 import { getFirestore } from 'firebase/firestore'; 
 
-const firebaseConfig = {
-    //Api_Key
-    authDomain: "hra-coventry.firebaseapp.com",
-    projectId: "hra-coventry",
-    storageBucket: "hra-coventry.appspot.com",
-    messagingSenderId: "334587329321",
-    //Api_Id
-    measurementId: "G-K4FFMVKQLZ"
-};
-
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app);
